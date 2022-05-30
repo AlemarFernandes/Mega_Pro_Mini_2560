6ee7c1a5dcc31676e117b94c93df05e8a604f2d9
Alemar Fernandes F Jr <alemarfernandes.jr@gmail.com>
2022-05-29 23:03:05 -0300

    Adicionado arquivo backUp

---
+++ Display7seg/Mega2560_Display7seg/Mega2560_Display7seg/main.c
@@ -0,0 +1,18 @@
+/*
+ * Mega2560_Display7seg.c
+ *
+ * Created: 29/05/2022 22:49:15
+ * Author : A_Fernandes
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
